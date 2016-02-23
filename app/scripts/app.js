d16c3628ab54cbea609f60b6e4320349414eaaa2
Fernando De Freitas <fdefreitas6@gmail.com>
2016-02-23 13:52:59 +0000

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name geckometerApp
+ * @description
+ * # geckometerApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('geckometerApp', [
+    'ngAnimate',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name geckometerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the geckometerApp
+ */
+angular.module('geckometerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
